f9be2b039cf101054925dc571b51b6e830bf09fb
Chris Rudder <cbrudder84@gmail.com>
2019-03-18 12:52:34 -0400

    deployed to heroku

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ var cookieParser = require('cookie-parser');
 // var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
-import DATABASE_URL from './config/database.js';
-import CLIENT_ORIGIN from './config/server.js';
+var {DATABASE_URL} = require('./config/database.js');
+var {CLIENT_ORIGIN} = require('./config/server.js');
 
 app.locals.db = mongoose.connect(DATABASE_URL, {
     useNewUrlParser: true
@@ -64,3 +64,30 @@ if (require.main === module){
 
 module.exports = { app, DATABASE_URL };
 
+let server;
+
+
+function runServer(DATABASE_URL, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DATABASE_URL, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// `closeServer` function is here in original code
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+};
